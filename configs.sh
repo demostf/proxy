0370bdd40bb77eccbf8c9f8f30ae682ff817bc4e
Robin Appelman <robin@icewind.nl>
2018-09-19 20:31:52 +0200

    add sync to proxy

--- configs.sh
+++ configs.sh
@@ -3,3 +3,4 @@
 sed -e "s/\${SOURCE}/$HOST/" -e "s/\${TARGET}/$TARGET/" nginx.conf > /etc/nginx/conf.d/$HOST.conf.nokey
 sed -e "s/\${SOURCE}/api.$HOST/" -e "s/\${TARGET}/$TARGET/" nginx.conf > /etc/nginx/conf.d/api.$HOST.conf.nokey
 sed -e "s/\${SOURCE}/static.$HOST/" -e "s/\${TARGET}/$TARGET/" nginx.conf > /etc/nginx/conf.d/static.$HOST.conf.nokey
+sed -e "s/\${SOURCE}/sync.$HOST/" -e "s/\${TARGET}/$TARGET/" nginx.conf > /etc/nginx/conf.d/sync.$HOST.conf.nokey
